c88006bb4ba5176d9c56b19bb23cc399c8555c51
msenih <senih1999@gmail.com>
2020-10-20 14:33:23 -0500

    Optimized some code and some more spring cleaning :)

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
@@ -11,13 +10,5 @@ public class MusicPlayer : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    void Start()
-    {
-        Invoke("LoadFirstScene", 10f);
-    }
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
-    }
+    
 }
