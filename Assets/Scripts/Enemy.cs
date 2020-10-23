94a49a33cbbc275944f687652145d16aa1aa83b8
msenih <senih1999@gmail.com>
2020-10-23 15:47:00 -0500

    Created and empty game object called Spawned At Runtime that instantiates enemy death fx and destroys them after a specified amount of time. Added design levers for fx destruction delay.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,9 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
+        
     }
 }
